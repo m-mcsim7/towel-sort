e70032b210b25d749b508e523a088a742d002b79
m-mcsim7 <m-mcsim7@yandex.by>
2022-01-25 10:11:37 +0300

    complite task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (matrix == undefined) return [];
+    else {
+        let matrixRevers = [matrix[0]];
+
+        for (let i = 1; i < matrix.length; i++) {
+            if (i % 2 != 0) {
+                matrix[i] = matrix[i].reverse();
+                //matrixRevers.push(matrix[i]);
+            }
+
+            // else {
+            //    matrix[i] = matrix[i];
+            //   // matrixRevers.push(matrix[i]);
+
+            //}
+        }
+
+        return matrix.flat();
+    }
+};
